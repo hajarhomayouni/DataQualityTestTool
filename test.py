736af0867c96553302f4031474d1c14e81e3151f
Homayouni <hhajar@CS-WIN.COLOSTATE.EDU>
2018-10-30 10:28:11 -0600

    Improve performace - test.py

--- test.py
+++ test.py
@@ -18,35 +18,35 @@ print dataFrame
 #Read csv data
 dataFrame=dataCollection.importData("testData.csv").head(100)
 
-#Preprocess data - The first column is assumed to be an ID column
-dataFramePreprocessed= dataCollection.preprocess(dataFrame.drop([dataFrame.columns.values[0]], axis=1), ['gender_concept_id','measurement_type_concept_id'], ['year_of_birth','value_as_number','range_low','range_high'])       
+#Preprocess data 
+dataFramePreprocessed= dataCollection.preprocess(dataFrame, ['gender_concept_id','measurement_type_concept_id'], ['year_of_birth','value_as_number','range_low','range_high'])       
 
 
-#Tune and Train model
+#Tune and Train model - The first column is assumed to be an ID column
 autoencoder = Autoencoder()
 hiddenOpt = [[50,50],[100,100], [5,5,5],[50,50,50]]
 l2Opt = [1e-4,1e-2]
 hyperParameters = {"hidden":hiddenOpt, "l2":l2Opt}
-bestModel=autoencoder.tuneAndTrain(hyperParameters,H2OAutoEncoderEstimator(activation="Tanh", ignore_const_cols=False, epochs=100),dataFramePreprocessed)
+bestModel=autoencoder.tuneAndTrain(hyperParameters,H2OAutoEncoderEstimator(activation="Tanh", ignore_const_cols=False, epochs=100),dataFramePreprocessed.drop([dataFramePreprocessed.columns.values[0]], axis=1))
 
-#Assign invalidity scores
-invalidityScores=autoencoder.assignInvalidityScore(bestModel, dataFramePreprocessed)
+#Assign invalidity scores - The first column is assumed to be an ID column
+invalidityScores=autoencoder.assignInvalidityScore(bestModel, dataFramePreprocessed.drop([dataFramePreprocessed.columns.values[0]], axis=1))
 
 #Detect faulty records
 testing=Testing()
-outlierFrame=testing.detectFaultyRecords(dataFrame, invalidityScores, np.median(invalidityScores))
-outlierFramePreprocessed=dataCollection.preprocess(outlierFrame.drop([outlierFrame.columns.values[0],'invalidityScore'],axis=1), ['gender_concept_id','measurement_type_concept_id'], ['year_of_birth','value_as_number','range_low','range_high']) 
+outlierFrame=testing.detectFaultyRecords(dataFramePreprocessed, invalidityScores, np.median(invalidityScores))
+
+#Preprocess faulty records
+#outlierFramePreprocessed=dataCollection.preprocess(outlierFrame.drop([outlierFrame.columns.values[0],'invalidityScore'],axis=1), ['gender_concept_id','measurement_type_concept_id'], ['year_of_birth','value_as_number','range_low','range_high']) 
 
 
 #Cluster the faulty records
-#Train a 5*5 SOM with 100 iterations
 #Exclude id columnand invalidity score for clustering
 som = SOM(5,5, len(outlierFrame.columns.values)-2, 400)
-som.train(outlierFramePreprocessed.values) 
+som.train(outlierFrame.drop([outlierFrame.columns.values[0],'invalidityScore'],axis=1).values) 
 #Map data to their closest neurons
-mapped= som.map_vects(outlierFramePreprocessed.values)
+mapped= som.map_vects(outlierFrame.drop([outlierFrame.columns.values[0],'invalidityScore'],axis=1).values)
 labels_list=[list(x) for x in mapped]
-print labels_list
 
 groups_of_outliers=[]
 group_index=0
@@ -59,6 +59,3 @@ for i in range(5):
             group_index+=1
 
 
-print groups_of_outliers
-print "*********************"
-print groups_of_outliers[0]
