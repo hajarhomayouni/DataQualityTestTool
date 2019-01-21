c9deba38716060f32d0cc9236131deb9666308f5
Hajar Homayouni <homayouni.hajar@gmail.com>
2019-01-21 10:53:24 -0700

    Add preporocess2 for one-hot encoding

--- backendClasses/DataCollection.py
+++ backendClasses/DataCollection.py
@@ -39,6 +39,35 @@ class DataCollection:
             dataFrame[col]=le.transform(dataFrame[col])
         dataFrame[dataFrame.columns.values]=min_max.fit_transform(dataFrame[dataFrame.columns.values])"""       
         return dataFrame
+    
+    def preprocess2(df_data):
+        categorical_columns=[]
+        for column in df_data.columns:
+            if df_data[column].dtype != np.number:
+                df_data[column]=df_data[column].apply(hash)
+            if all(float(x).is_integer() for x in df_data[column]):
+                categorical_columns.append(column)   
+
+        #preprocess categorical columns
+        df_data_1=df_data.drop(categorical_columns, axis=1)   
+        enc=OneHotEncoder(sparse=False) 
+        for col in categorical_columns:
+            # creating an exhaustive list of all possible categorical values
+            data=df_data[[col]]
+            enc.fit(data)
+            # Fitting One Hot Encoding on train data
+            temp = enc.transform(df_data[[col]])
+            # Changing the encoded features into a data frame with new column names
+            temp=pd.DataFrame(temp,columns=[(col+"_"+str(i)) for i in data[col]
+                                            .value_counts().index])
+            # In side by side concatenation index values should be same
+            # Setting the index values similar to the X_train data frame
+            temp=temp.set_index(df_data.index.values)
+            # adding the new One Hot Encoded varibales to the train data frame
+            df_data_1=pd.concat([df_data_1,temp],axis=1)
+        return df_data_1
+    
+    
     @staticmethod
     def selectFeatures(dataFrame):
         #use feature selection/prioritization methods
