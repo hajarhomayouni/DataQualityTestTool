d6803edecc041e4b8b18d3a339031cc78426ea29
hajarhomayouni <hhajar@colostate.edu>
2019-04-12 16:00:16 +0000

    test double loads in html pages

--- DQTestTool.py
+++ DQTestTool.py
@@ -66,163 +66,16 @@ def importDataFrame():
 
 @bp.route('/validate', methods=["GET","POST"])
 def validate():
-    print "1"
-    #Initializations
-    datasetId=request.args.get('datasetId')
-    trainedModelFilePath=request.args.get('trainedModelFilePath')
-    db=get_db()
-    interpretationMethod=request.args.get('interpretationMethod')
-    clusteringMethod=request.args.get('clusteringMethod')
-    truePositive=0.0
-    truePositiveRate=0.0
-    dataFrame=pd.read_sql(sql="SELECT * FROM dataRecords_"+datasetId, con=db)
-    #select actual faluts from previous run before updating the database - we need this information to measure the false negative rate
-    AFdataFrameOld=pd.read_sql(sql="select "+dataFrame.columns.values[0]+" from dataRecords_"+datasetId+" where status like 'actualFaults_%' or status like 'suspicious_%'", con=db)
-
-    dataCollection=DataCollection()
-    #remove column id and status for analysis
-    dataFramePreprocessed=dataCollection.preprocess(dataFrame.drop([dataFrame.columns.values[0], dataFrame.columns.values[-1]], axis=1))
-
-    #Prepare Training data: Remove actual faults from training data; change their status from clean to actualFaults_i   
-    if request.method == "POST":
-     print "2"
-     numberOfClusters=request.form["numberOfClusters"]
-     if numberOfClusters:
-        for i in range(int(numberOfClusters)):
-            if i in request.form.getlist('Group'):
-                db.execute("Update dataRecords_"+datasetId+" set status='actualFaults_"+str(i)+ "' where status='suspicious_"+str(i)+"'")
-            else:
-                db.execute("Update dataRecords_"+datasetId+" set status='clear' where status='suspicious_"+str(i)+"'")
-
-    print "3"
-    #select actual faluts from the current run after updating the database - we need this information to measure the false negative rate
-    AFdataFrame=pd.read_sql(sql="select "+dataFrame.columns.values[0]+" from dataRecords_"+datasetId+" where status like 'actualFaults_%'", con=db)
-
-    dataFrameTrain=pd.read_sql(sql="SELECT * FROM dataRecords_"+datasetId+ " where status='clean' or status like 'suspicious_%'", con=db)
-    dataFrameTrainPreprocessed=dataCollection.preprocess(dataFrameTrain.drop([dataFrameTrain.columns.values[0], dataFrameTrain.columns.values[-1]], axis=1))
-    
-    #Tune and Train model
-    h2o.init()   
-    autoencoder = Autoencoder()
-    bestConstraintDiscoveryModel=H2OAutoEncoderEstimator()
-    if trainedModelFilePath!="":
-        bestConstraintDiscoveryModel = h2o.load_model(trainedModelFilePath)
-    else:
-        hiddenOpt = [[50,50],[100,100], [5,5,5],[50,50,50]]
-        l2Opt = [1e-4,1e-2]
-        hyperParameters = {"hidden":hiddenOpt, "l2":l2Opt}
-        bestConstraintDiscoveryModel=autoencoder.tuneAndTrain(hyperParameters,H2OAutoEncoderEstimator(activation="Tanh", ignore_const_cols=False, epochs=200,standardize = True,categorical_encoding='auto',export_weights_and_biases=True, quiet_mode=False),dataFrameTrainPreprocessed)
-
-    #Assign invalidity scores per feature
-    invalidityScoresPerFeature=autoencoder.assignInvalidityScorePerFeature(bestConstraintDiscoveryModel, dataFramePreprocessed)
-    
-    #Assign invalidity scores per record
-    #based on average of attribute's invalidity scores
-    #invalidityScores=autoencoder.assignInvalidityScore(bestConstraintDiscoveryModel, dataFramePreprocessed)
-    #based on max of attribute's invalidity scores
-    invalidityScores=invalidityScoresPerFeature.max(axis=1).values.ravel()
-    
-    #concat record id to the invalidity score per feature
-    invalidityScoresPerFeature= pd.concat([dataFrame[dataFrame.columns[0]], invalidityScoresPerFeature], axis=1, sort=False)
-    
-    #Detect faulty records
-    testing=Testing()  
-    faultyRecordFrame=testing.detectFaultyRecords(dataFrame, invalidityScores,np.percentile(invalidityScores,95))#statistics.mean(invalidityScores))#nnp.percentile(invalidityScores,0.5))
-    #Detect faulty records based on invalidity scores
-    faultyInvalidityScoreFrame=testing.detectFaultyRecords(invalidityScoresPerFeature,invalidityScores,np.percentile(invalidityScores,95))#,statistics.mean(invalidityScores))#,np.percentile(invalidityScores,0.5))
-    #Detect normal records
-    normalRecordFrame=testing.detectNormalRecords(dataFrame,invalidityScores,np.percentile(invalidityScores,50))#,statistics.mean(invalidityScores))#,np.percentile(invalidityScores,0.5))
-    #normalRecordFrame=testing.detectNormalRecordsBasedOnFeatures(dataFrame, invalidityScoresPerFeature, invalidityScores,np.percentile(invalidityScores,0.5))#sum(invalidityScores)/len(invalidityScores))
-
-    #store different scores in database for this run
-    if not AFdataFrame.empty:
-        truePositiveRate=float(len(AFdataFrame[dataFrame.columns.values[0]].unique().tolist()))/float(faultyRecordFrame.shape[0])
-        falsePositiveRate=1.0-truePositiveRate
-        falseNegativeRate=0.0
-
-        if not AFdataFrameOld.empty:
-            falseNegativeRate=len(set(AFdataFrameOld[dataFrame.columns.values[0]].unique().tolist()).difference(set(AFdataFrame[dataFrame.columns.values[0]].unique().tolist())))/len(set(AFdataFrameOld[dataFrame.columns.values[0]].unique().tolist()))
-        trueNegativeRate=1-falseNegativeRate
-        db.execute('INSERT INTO scores (time, dataset_id, true_positive_rate, false_positive_rate, true_negative_rate, false_negative_rate) VALUES (?, ?, ?, ?, ?,?)',(datetime.datetime.now(), datasetId, truePositiveRate, falsePositiveRate,trueNegativeRate, falseNegativeRate))
-
-    #Cluster the faulty records
-    #If you want to work with data directly for clustering, use faultyRecordFrame directly. Now it clusters based on invelidity score per feature
-    dataFrames=[]
-    if clusteringMethod=="som":
-        som = SOM(4,4, len(faultyInvalidityScoreFrame.columns.values)-2, 50)
-        dataFrames=som.clusterFaultyRecords(faultyInvalidityScoreFrame.drop([faultyInvalidityScoreFrame.columns.values[0],'invalidityScore'],axis=1), faultyRecordFrame)
-    elif clusteringMethod=="kprototypes":
-        kmeans=H2oKmeans()
-        bestClusteringModel=kmeans.tuneAndTrain(faultyRecordFrame.drop([faultyRecordFrame.columns.values[0],'invalidityScore'],axis=1))
-        dataFrames=kmeans.clusterFaultyRecords(bestClusteringModel,faultyRecordFrame.drop([faultyRecordFrame.columns.values[0],'invalidityScore'],axis=1), faultyRecordFrame)
-    #
-    
-    #Update status of suspicious groups in database
-    i=0
-    for dataFrame in dataFrames:
-        dataFrame.to_sql('suspicious_i_temp_'+datasetId, con=db, if_exists='replace', index=False)
-        db.execute("Update dataRecords_"+datasetId+" set status='suspicious_"+str(i)+ "' where "+dataFrame.columns.values[0]+" in (select "+dataFrame.columns.values[0]+ " from suspicious_i_temp_"+datasetId+")")
-        db.execute("Drop table suspicious_i_temp_"+datasetId)       
-        i=i+1
-    numberOfClusters=i
-    faulty_records_html=[]
-    cluster_scores_fig_url=[]
-    cluster_dt_url=[]
-    cluster_interpretation=[]
-    treeRules=[]
-    
-    #show the suspicious groups as HTML tables
-    for i in range(int(numberOfClusters)):
-        faulty_records=dataFrames[i]
-        faulty_records_html.append(faulty_records.to_html())
-        
-        #Show descriptive graph for each group
-        cluster_scores=invalidityScoresPerFeature.loc[invalidityScoresPerFeature[dataFrame.columns.values[0]].isin(faulty_records[dataFrame.columns.values[0]])]
-        #exclude status and invalidityScore columns
-        X=dataFrame.columns.values[1:-2]
-        Y=cluster_scores.mean().tolist()[1:]
-        cluster_scores_fig_url.append(dataCollection.build_graph(X,Y))
-
-        #indicate the attributes with high invalidity score values
-        faulty_attributes_indexes=[i for i,v in enumerate(Y) if v > np.percentile(Y,90)]
-        faulty_attributes=X[faulty_attributes_indexes]
-        
-        #Interpret each cluster
-        normalRecordFrame['label']='valid'
-        faulty_records['label']='suspicious'
-        decisionTreeTrainingFrame= pd.concat([normalRecordFrame,faulty_records])
-        decisionTreeTrainingFramePreprocessed=dataCollection.preprocess(decisionTreeTrainingFrame)
-        tree=H2oGradientBoosting()
-        if interpretationMethod=="Sklearn Decision Tree":
-            tree=SklearnDecisionTree()
-        if interpretationMethod=="Sklearn Random Forest":
-            tree=SklearnRandomForest()
-        if interpretationMethod=="H2o Random Forest":
-            tree=H2oRandomForest()
-        
-        treeModel=tree.train(decisionTreeTrainingFramePreprocessed,faulty_attributes,'label' )
-        cluster_dt_url.append(tree.visualize(treeModel,faulty_attributes,['valid','suspicious']))
-        treeCodeLines=tree.treeToCode(treeModel,faulty_attributes)
-        treeRules.append(tree.treeToRules(treeModel,faulty_attributes))
-        cluster_interpretation.append(tree.interpret(treeCodeLines))
+    data='fisrt load'
        
     if request.method == 'POST':
-        print "4"
-        knownFaults=""
-        if request.files:
-            f = request.files['file']
-            knownFaults='datasets/PD/'+f.filename
-            f.save(knownFaults)
         if request.form.get('revalidate'):
-            trainedModelFilePath=""
-            return redirect(url_for('DQTestTool.validate',trainedModelFilePath=trainedModelFilePath, datasetId=datasetId, interpretationMethod=interpretationMethod, clusteringMethod=clusteringMethod))
+            data='second load'
+            return redirect(url_for('DQTestTool.validate'))
         if request.form.get('evaluation'):
             return redirect(url_for('DQTestTool.evaluation', datasetId=datasetId, knownFaults=knownFaults))
 
-    bestModelPath = h2o.save_model(model=bestConstraintDiscoveryModel, path="static/model/", force=True)         
-    bestModelFileName=bestModelPath.split('/')[len(bestModelPath.split('/'))-1]
-    print "5"
-    return render_template('validate.html', data='@'.join(faulty_records_html), datasetId=datasetId, numberOfClusters=numberOfClusters, fig_urls=cluster_scores_fig_url,cluster_dt_url=cluster_dt_url, cluster_interpretation=cluster_interpretation, treeRules=treeRules, bestModelFile='/static/model/'+bestModelFileName)
+    return render_template('validate.html', data=data)
      
 @bp.route('/evaluation', methods=["GET","POST"])
 def evaluation():
